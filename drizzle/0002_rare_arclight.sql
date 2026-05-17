ad7e152c8023159991a1b83d81d4ef8a786a2f7d
lam.ho <holam9120@gmail.com>
2026-05-17 21:44:41 +0700

    feat: disable source refresh for a specific playlist

--- drizzle/0002_rare_arclight.sql
+++ drizzle/0002_rare_arclight.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "playlists" ADD COLUMN "auto_refresh_disabled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "sources" ADD COLUMN "last_refreshed_at" timestamp with time zone;
+ALTER TABLE "playlists" ADD COLUMN "auto_refresh_disabled" boolean DEFAULT false NOT NULL;
